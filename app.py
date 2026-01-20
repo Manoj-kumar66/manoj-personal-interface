e31f442a5a1929c17fa6a364bf0f4e7a3d70c943
Manoj-kumar66 <maajimanoj6@gmail.com>
2026-01-20 14:36:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask, render_template, send_from_directory, request, redirect, url_for, session, abort
+from flask import Flask, render_template, send_from_directory, request, session, redirect, abort
 import os
 
 app = Flask(__name__)
-app.secret_key = "manoj_secret_key_change_later"
+app.secret_key = "manoj_secret_key"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILES_DIR = os.path.join(BASE_DIR, "my_files")
 
-PASSWORD = "6666"   # 🔐 change this anytime
+PASSWORD = "6666"
+
 
 @app.route("/", methods=["GET", "POST"])
-def login():
+def main():
     if request.method == "POST":
         if request.form.get("password") == PASSWORD:
             session["auth"] = True
-            return redirect("/panel")
-        return render_template("login.html", error="ACCESS DENIED")
-    return render_template("login.html")
+            return redirect("/")
+        else:
+            return render_template("login.html", error="ACCESS DENIED")
 
-@app.route("/panel")
-def panel():
-    if not session.get("auth"):
-        return redirect("/")
-    files = os.listdir(FILES_DIR)
-    return render_template("index.html", files=files)
+    files = []
+    if session.get("auth"):
+        files = os.listdir(FILES_DIR)
+
+    return render_template("login.html", files=files)
+
 
 @app.route("/files/<path:filename>")
 def open_file(filename):
@@ -31,10 +32,29 @@ def open_file(filename):
         abort(403)
     return send_from_directory(FILES_DIR, filename)
 
+
+@app.route("/preview/<path:filename>")
+def preview_file(filename):
+    if not session.get("auth"):
+        abort(403)
+
+    file_path = os.path.join(FILES_DIR, filename)
+
+    if not os.path.isfile(file_path):
+        abort(404)
+
+    if not filename.lower().endswith(".txt"):
+        return "Preview supported only for .txt files"
+
+    with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+        return f.read()
+
+
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
