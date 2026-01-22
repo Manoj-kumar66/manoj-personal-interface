c2c610be49c203fda0abcae84378058a08eacbf6
Manoj-kumar66 <maajimanoj6@gmail.com>
2026-01-22 21:29:24 +0530

    Implement PIN login functionality
    Refactor login function to include PIN verification and redirect.

--- static/script.js
+++ static/script.js
@@ -1,31 +1,9 @@
-let started = false;
+function login() {
+    const pin = document.getElementById("pin").value;
 
-document.addEventListener("click", () => {
-    if (!started) {
-        started = true;
-
-        const sound = new Audio("/static/startup.mp3");
-        sound.volume = 0.7;
-        sound.play().catch(err => console.log(err));
-
-        startTyping();
+    if (pin === "1234") {   // change your pin
+        window.location.href = "/dashboard";
+    } else {
+        alert("ACCESS DENIED");
     }
-});
-
-function startTyping() {
-    const text = "SYSTEM BOOTING...\nAI CORE ONLINE\nWELCOME MANOJ";
-    let i = 0;
-    const speed = 50;
-
-    const output = document.getElementById("terminal");
-
-    function type() {
-        if (i < text.length) {
-            output.textContent += text.charAt(i);
-            i++;
-            setTimeout(type, speed);
-        }
-    }
-
-    type();
 }
