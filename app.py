3201a06d9b4b75a599efea0d46660a52e16d89df
Manoj-kumar66 <maajimanoj6@gmail.com>
2026-01-22 23:28:39 +0530

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
 
 app = Flask(__name__)
 
@@ -9,7 +8,6 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        # simple demo login
         if username == "manoj" and password == "6666":
             return redirect(url_for("dashboard"))
         else:
@@ -20,7 +18,3 @@ def login():
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
